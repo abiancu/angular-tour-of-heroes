81b41d921af7f1f07112a3ce2b3d0db713ad51c3
abiancu <alejandrobiancucci@gmail.com>
2018-02-11 02:01:27 -0600

    Obrservable
    created observables and updated the heroes.component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from '@angular/core';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/Observable/of';
  
 @Injectable()
 export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[]{  // < -- RETURN MOCK-HEROES 
-  return HEROES; 
+  // getHeroes(): Hero[]{  // < -- RETURN MOCK-HEROES 
+  // return HEROES; 
+  // }
+
+  getHeroes(): Observable<Hero[]>{
+    return of(HEROES);
   }
 
 }
