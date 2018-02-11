3bc5b28fec805d3175d725d76d7627f5abef4c1c
abiancu <alejandrobiancucci@gmail.com>
2018-02-11 01:41:09 -0600

    created services
    geHeros() to call heroes, updated HeroesComponents

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero'
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes'; //<-- don't need it becuase I imported HeroServices below.
+import { HeroService } from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +12,8 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  // heroes = HEROES; //<-- this won't work, replce with the line below:
+  heroes: Hero[];
 
   selectedHero: Hero;
   
@@ -18,13 +22,18 @@ export class HeroesComponent implements OnInit {
   //     name: 'Alejandro'
   //   };
 
-  constructor() { }
+  constructor(private heroService: HeroService) { } //<-- To inject the HeroService
 
   ngOnInit() {
+    this.getHeroes(); 
+    
   }
 
-  onSelected(hero:Hero): void{
+  onSelect(hero:Hero): void{
     this.selectedHero=hero;
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
